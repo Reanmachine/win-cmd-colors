b729fd1b48bf2a9af86f4c0ad153510d988b01c3
Aren Blondahl <rean1985@gmail.com>
2016-10-25 22:02:57 -0600

    Added a test batch file

---
+++ Test.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+echo [0mForeground:
+echo [30m00[31m01[32m02[33m03[34m04[35m05[36m06[37m07[0m
+echo [90m08[91m09[92m0A[93m0B[94m0C[95m0D[96m0E[97m0F[0m
+echo [0mBackground:
+echo [40m00[41m10[42m20[43m30[44m40[45m50[46m60[47m70[0m
+echo [100m80[101m90[102mA0[103mB0[104mC0[105mD0[106mE0[107mF0[0m
